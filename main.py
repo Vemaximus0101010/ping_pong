6f78a0428b7a78f1b01d780c8167d64781de6a6c
Max Medvedev <maksimmeddve@gmail.com>
2025-02-02 09:58:37 +0300

    Update main.py
    The main code

--- main.py
+++ main.py
@@ -4,6 +4,17 @@ import pygame_menu
 init()
 mixer.init()
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, p_image, p_w, p_h, p_x, p_y, p_speed):
+        sprite.Sprite.__init__(self)
+        self.image = transform.scale(image.load(p_image), (p_w, p_h))
+        self.rect = self.image.get_rect()
+        self.rect.x = p_x
+        self.rect.y = p_y
+        self.p_speed = p_speed
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
 w, h = 1200, 1000
 
 window = display.set_mode((w, h))
